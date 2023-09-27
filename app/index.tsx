8cc6eeba416d4c3af9a9343429a1a866a5dd2782
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-09-27 12:07:55 +0700

    test

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MyApp() {
     return (
       <div>
-          <h1>Welcome to my app</h1>
+        <h1>Welcome to my app</h1>
   </div>
     );
   }
