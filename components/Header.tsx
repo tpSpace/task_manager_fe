26066008f364bf8b90a084a0f6537dd4b3d1be9a
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-10-04 13:43:27 +0700

    Khang | add | add basic structure for the home page

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+'use client';
+import React from 'react';
+
+import CustomLink from './CustomLink';
+
+const Header = () => {
+  return (
+    <div className="flex w-screen m-4">
+      <div className="flex justify-start w-6/12">
+        <CustomLink
+          containerStyles="justify-self-start border-2 border-solid rounded-lg p-2"
+          route="/home"
+          title="Home"
+        />
+      </div>
+      <div className="flex justify-end w-6/12">
+        <CustomLink
+          containerStyles="justify-self-end mr-10 border-2 border-solid rounded-lg p-2"
+          route="/login"
+          title="Login"
+        />
+        <CustomLink
+          containerStyles="justify-self-end mr-10 border-2 border-solid rounded-lg p-2"
+          route="/register"
+          title="Register"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
