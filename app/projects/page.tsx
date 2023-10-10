0dc743f2b636d4c3ca6a9af9a195037d80821ff9
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-10-10 19:15:07 +0700

    Khang | add | add datatypes

---
+++ app/projects/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React from 'react';
+
+import CustomButton from '@/components/CustomButton';
+import SingleProject from '@/components/SingleProject';
+import { ProjectProps } from '@/types';
+
+const Projects = () => {
+  const projects: ProjectProps[] = [];
+
+  return (
+    <div className="flex justify-center">
+      <CustomButton containerStyles="border border-2 border-solid" title="+" />
+      {projects.map((project, index) => (
+        <SingleProject
+          admin={project.admin}
+          dateCreate={project.dateCreate}
+          id={project.id}
+          key={index}
+          members={project.members}
+          stages={project.stages}
+          title={project.title}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
