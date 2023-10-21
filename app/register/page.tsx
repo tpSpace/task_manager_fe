40848c10bdd8088dfa1759c2cd8d1aba01b8446f
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-10-21 23:04:16 +0700

    Add react-hook-form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import React from 'react';
 
 const RegisterPage = () => {
   return (
     <div className="flex justify-center items-center h-[90%] w-full">
-      <p>s</p>
+      <div>
+        <form>
+          <input type="text">Username</input>
+          <input type="text">Email</input>
+          <input type="password">Password</input>
+          <input type="password">Confirm Password</input>
+          <input type="submit" value="submit" />
+        </form>
+      </div>
     </div>
   );
 };
