485a5a66c611e4f27a7e2bd347d064569580dffb
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-10-26 10:54:15 +0700

    Khoi | Edit | add confirmPassword

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,28 +1,42 @@
 'use client';
 
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import { registerSchemaType } from '../../types/inputValidate';
 
 const RegisterPage = () => {
+  const { register, handleSubmit } = useForm<registerSchemaType>({});
+
   return (
     <div className="flex justify-center items-center h-[90%] w-full">
       <div className="border-2 boder-solid border-black">
-        <form className="flex flex-col m-4 gap-4">
+        <form
+          className="flex flex-col m-4 gap-4"
+          onSubmit={handleSubmit(data => {
+            console.log(data);
+          })}
+        >
           <input
+            {...register('username')}
             className="border-2 border-black p-1"
             placeholder="username"
             type="text"
           />
           <input
+            {...register('email')}
             className="border-2 border-black p-1"
             placeholder="email"
             type="email"
           />
           <input
+            {...register('password')}
             className="border-2 border-black p-1"
             placeholder="password"
             type="password"
           />
           <input
+            {...register('confirmPassword')}
             className="border-2 border-black p-1"
             placeholder="retype-password"
             type="password"
@@ -31,7 +45,7 @@ const RegisterPage = () => {
             className="border-2 border-black bg-white hover:bg-black hover:text-white"
             type="submit"
           >
-            Login
+            Register
           </button>
         </form>
       </div>
