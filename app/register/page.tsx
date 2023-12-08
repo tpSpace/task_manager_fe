5a8b65f8dc749dafa11e32ba91cfde6352514cf4
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-08 09:54:51 +0700

    Khoi | add | add api to register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
 
 import { registerSchema, registerSchemaType } from '../../types/inputValidate';
 
@@ -12,14 +13,35 @@ const RegisterPage = () => {
     resolver: zodResolver(registerSchema),
   });
 
+  const submit = async (data: registerSchemaType) => {
+    console.log(data);
+    await axios
+      .post('http://localhost:3001/auth/register', {
+        name: data.name,
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      })
+      .then(res => {
+        console.log(res);
+        if (res.data.status === 'success') {
+          console.log(res);
+          localStorage.setItem('token', res.data.token);
+          window.location.href = '/projects';
+          console.log(res.data.token);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="flex justify-center items-center h-[90%] w-full">
       <div className="border-2 boder-solid border-black">
         <form
           className="flex flex-col m-4 gap-4"
-          onSubmit={handleSubmit(data => {
-            console.log(data);
-          })}
+          onSubmit={handleSubmit(submit)}
         >
           <input
             {...register('username')}
--- types/inputValidate.ts
+++ types/inputValidate.ts
@@ -20,6 +20,7 @@ export const loginSchema = z.object({
 });
 
 export const registerSchema = z.object({
+  name: z.string().min(3).max(255),
   username: z.string().min(3).max(255),
   email: z.string().email().trim(),
   password: z.string().min(8).max(100).trim(),
