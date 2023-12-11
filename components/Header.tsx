6f8ba9703f8244dac357c5f8e81d7ab79436cd9e
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-12 01:19:54 +0700

    Khoi | Add | Implement simple logout navBar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,23 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import CustomLink from './CustomLink';
 
 const Header = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLogged(true);
+    }
+  }, []);
+
+  function logout() {
+    console.log('logout');
+    localStorage.removeItem('token');
+    setIsLogged(false);
+  }
+
   return (
     <div className="flex flex-row justify-between items-center p-4 overflow-hidden h-[10%]">
       <div className="">
@@ -13,18 +27,36 @@ const Header = () => {
           title="Home"
         />
       </div>
-      <div className="flex gap-4">
-        <CustomLink
-          containerStyles="border-2 border-solid border-black rounded-lg p-2 hover:bg-black hover:text-white"
-          route="/login"
-          title="Login"
-        />
-        <CustomLink
-          containerStyles="border-2 border-solid border-black rounded-lg p-2 hover:bg-black hover:text-white"
-          route="/register"
-          title="Register"
-        />
-      </div>
+      {!isLogged && (
+        <div className="flex gap-4">
+          <CustomLink
+            containerStyles="border-2 border-solid border-black rounded-lg p-2 hover:bg-black hover:text-white"
+            route="/login"
+            title="Login"
+          />
+          <CustomLink
+            containerStyles="border-2 border-solid border-black rounded-lg p-2 hover:bg-black hover:text-white"
+            route="/register"
+            title="Register"
+          />
+        </div>
+      )}
+      {isLogged && (
+        <div className="flex gap-4">
+          <CustomLink
+            containerStyles="border-2 border-solid border-black rounded-lg p-2 hover:bg-black hover:text-white"
+            route="/projects"
+            title="Projects"
+          />
+          <button aria-hidden="true" onClick={logout}>
+            <CustomLink
+              containerStyles="border-2 border-solid border-black rounded-lg p-2 hover:bg-black hover:text-white"
+              route="/"
+              title="Logout"
+            />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
