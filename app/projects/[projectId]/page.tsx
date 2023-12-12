c9638863bac51ab14fae8e2af00c12c43beb69f3
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-12-12 14:11:41 +0700

    Khang | edit | edit fetch project and fetch tags functions

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -5,17 +5,31 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import SingleProject from '@/components/SingleProject';
-import { ProjectProps } from '@/types';
+import { ProjectProps, UserProps } from '@/types';
 
 interface ProjectDetailProps {
-  params: {
-    projectId: string;
-  };
+  projectId: string;
+  title: string;
+  stageIds: string[];
+  memberIds: string[];
+  adminId: string;
+  history: string[];
+  tagIds: string[];
 }
 
-const ProjectDetail = ({ params }: ProjectDetailProps) => {
+const ProjectDetail = ({ params }: { params: { projectId: string } }) => {
+  const [projectData, setProjectData] = useState<ProjectDetailProps>({
+    projectId: params.projectId,
+    title: '',
+    stageIds: [],
+    memberIds: [],
+    adminId: '',
+    history: [],
+    tagIds: [],
+  });
+
   const [project, setProject] = useState<ProjectProps>({
-    id: '',
+    id: params.projectId,
     title: '',
     stages: [],
     members: [],
@@ -29,34 +43,101 @@ const ProjectDetail = ({ params }: ProjectDetailProps) => {
     tags: [],
   });
 
+  // main useEffect, use for fetching the whole project
   useEffect(() => {
     const token = localStorage.getItem('token');
+
+    fetchTags(token);
     fetchProject(token);
   }, []);
 
-  const fetchProject = async (token: string | null) => {
+  const fetchProject = async (token: string | null): Promise<ProjectProps> => {
+    const res = await axios.get(
+      `http://localhost:3001/projects/get/${params.projectId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    console.log(res.data);
+    setProjectData(prevData => ({
+      ...prevData,
+      stageIds: res.data.project.stageIds,
+      memberIds: res.data.project.userIds,
+      adminId: res.data.project.adminId,
+      tagIds: res.data.project.tagsIds,
+    }));
+
+    setProject(prevProject => ({
+      ...prevProject,
+      title: res.data.project.title,
+      history: res.data.project.history,
+    }));
+
+    await fetchAdmin(token);
+    await fetchMembers(token);
+
+    console.log(projectData);
+    return res.data.project;
+  };
+
+  const fetchAdmin = async (token: string | null) => {
     await axios
-      .get(`http://localhost:3001/projects/get/${params.projectId}`, {
+      .get(`http://localhost:3001/user/${projectData.adminId}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
       .then(res => {
-        console.log(res);
-        setProject({
-          id: res.data.id,
-          title: res.data.title,
-          stages: res.data.stages,
-          members: res.data.members,
-          admin: {
-            userName: res.data.admin.userName,
-            id: res.data.admin.id,
-            avatar: res.data.admin.avatar,
-            token: res.data.admin.token,
+        console.log(res.data);
+        setProject(prevProject => ({
+          ...prevProject,
+          admin: res.data.user,
+        }));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const fetchMembers = async (token: string | null) => {
+    const members: UserProps[] = [];
+    projectData.memberIds.forEach(async memberId => {
+      await axios
+        .get(`http://localhost:3001/user/${memberId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
           },
-          history: res.data.history,
-          tags: res.data.tags,
+        })
+        .then(res => {
+          console.log(res.data);
+          members.push(res.data.user);
+        })
+        .catch(err => {
+          console.log(err);
         });
+    });
+
+    console.log(members);
+    setProject(prevProject => ({
+      ...prevProject,
+      members: members,
+    }));
+  };
+
+  const fetchTags = async (token: string | null) => {
+    await axios
+      .get(`http://localhost:3001/tags/get/${params.projectId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(res => {
+        setProject(prevProject => ({
+          ...prevProject,
+          tags: res.data.tag,
+        }));
       })
       .catch(err => {
         console.log(err);
@@ -64,7 +145,7 @@ const ProjectDetail = ({ params }: ProjectDetailProps) => {
   };
 
   return (
-    <div className="flex justify-center font-bold text-2xl">
+    <div className="flex flex-col items-center font-bold text-2xl">
       Project {params.projectId} Detail
       <SingleProject project={project} />
     </div>
--- components/SingleProject.tsx
+++ components/SingleProject.tsx
@@ -10,6 +10,17 @@ const SingleProject = ({ project }: SingleProjectProps) => {
   return (
     <div>
       <h1>{project.title}</h1>
+      <div>
+        {project.members?.map((member, index) => (
+          <p key={index}>member's name: {member.userName}</p>
+        ))}
+      </div>
+
+      <div>
+        {project.tags?.map((tag, index) => (
+          <p key={index}>tag's name: {tag.title}</p>
+        ))}
+      </div>
     </div>
   );
 };
