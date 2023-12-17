a18971834aed58599fc16140d801ae0047e9d571
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-17 23:32:06 +0700

    Khoi | Edit | replace logo with .svg

--- components/AddProjectPopUp.tsx
+++ components/AddProjectPopUp.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-interface PopupProps{
+interface PopupProps {
   trigger: boolean;
-  setTrigger: (state:boolean)=>void;
+  setTrigger: (state: boolean) => void;
   children: any;
 }
 
-const AddProjectPopUp = (props:PopupProps) => {
-  return (props.trigger)? (
-    <div>
-
-    </div>
-  ): (<div></div>);
-}
+const AddProjectPopUp = (props: PopupProps) => {
+  return props.trigger ? <div></div> : <div></div>;
+};
