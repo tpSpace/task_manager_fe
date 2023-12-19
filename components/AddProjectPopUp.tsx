22261025588f3a67d240c93a70ccb46bce7aecd7
Nguyễn Mạch Khang Huy <itcsiu21072@student.hcmiu.edu.vn>
2023-12-20 01:04:16 +0700

    Huy|Add|Delete project function

--- components/AddProjectPopUp.tsx
+++ components/AddProjectPopUp.tsx
@@ -8,7 +8,7 @@ interface PopupProps {
 
 const AddProjectPopUp = (props: PopupProps) => {
   return props.trigger ? (
-    <div className="fixed top-0 left-0 w-full h-[100vh] flex justify-center items-center backdrop-blur">
+    <div className="fixed top-0 left-0 w-full h-[100vh] flex justify-center items-center backdrop-blur z-40">
       <div className="w-full h-full max-w-[640px] max-h-[480px] bg-black relative rounded-[20px] border-none">
         <button
           className="absolute top-[16px] right-[16px] border-none bg-transparent text-white h-[25px] w-[25px] text-[20px] font-bold -mt-[5px]"
