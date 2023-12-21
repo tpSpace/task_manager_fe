d9ccd2ba8660d9a472b49ce26d23d975aab85064
Nguyễn Tiến Hưng <quankun3357@gmail.com>
2023-12-21 21:36:37 +0700

    Hung | Create | Create Delete Function and Edit tittle

--- components/SingleStage.tsx
+++ components/SingleStage.tsx
@@ -2,11 +2,11 @@
 import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
+import { MdDelete } from 'react-icons/md';
 
 import TicketCard from './TicketCard';
 import TicketCreationForm from './TicketCreationForm';
 
-import ProjectDetail from '@/app/projects/[projectId]/page';
 import CustomButton from '@/components/CustomButton';
 import { StageProps } from '@/types';
 
@@ -18,12 +18,35 @@ interface SingleStageProps {
 
 const SingleStage: React.FC<SingleStageProps> = ({ stage, flag, setFlag }) => {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
   const [updateStage, setUpdatedStage] = useState<StageProps>({
     id: stage.id,
     title: stage.title,
     tickets: stage.tickets,
   });
+
+  const deleteStage = async () => {
+    const token = localStorage.getItem('token');
+    await axios
+      .delete(`${API_URL}/stages/delete/${stage.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(res => {
+        console.log(res.data);
+        console.log('Delete stage successfully');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const handleChangeStage = async (title: string) => {
+    setUpdatedStage(prevStage => ({
+      ...prevStage,
+      title: title,
+    }));
+  };
   useEffect(() => {
     loadStage();
   }, [flag]);
@@ -63,7 +86,21 @@ const SingleStage: React.FC<SingleStageProps> = ({ stage, flag, setFlag }) => {
   return (
     <div className="mt-20 mx-10 min-w-[20%] h-[80%] rounded-lg border border-black flex justify-between flex-col">
       <div className="h-12 relative -top-5 flex items-center justify-center border rounded-full bg-black text-white text-2xl">
-        {stage.title}
+        <input
+          className="absolute h-12 px-4 border rounded-full bg-black text-white text-2xl"
+          onBlur={e => {
+            setFlag();
+            handleChangeStage(e.target.value);
+          }}
+          onChange={e => handleChangeStage(e.target.value)}
+          value={updateStage.title}
+        />
+        <div className="relative left-[120px]">
+          <MdDelete
+            className="text-2xl hover:text-3xl cursor-pointer text-white"
+            onClick={deleteStage}
+          />
+        </div>
       </div>
       <div className="flex flex-col justify-between items-center overflow-x-hidden overflow-y-auto max-h-96">
         {stage.tickets?.map((ticket, index) => (
