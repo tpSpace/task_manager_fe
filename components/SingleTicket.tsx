6b33a6c78b07a61dd4040210a94e35ff64979179
Ho-Gia-Khang <hogiakhanglatoi@gmail.com>
2023-12-23 08:52:20 +0700

    Khang | add | add ticket pop up layout

--- components/SingleTicket.tsx
+++ components/SingleTicket.tsx
@@ -91,7 +91,7 @@ const SingleTicket = ({
       <Transition appear as={Fragment} show={isOpen}>
         <Dialog
           as="div"
-          className="relative border-black border-2 h-0 inset-0"
+          className="relative z-10 h-0 inset-0"
           onClose={closeModal}
         >
           {/* Ticket creation form */}
@@ -110,8 +110,8 @@ const SingleTicket = ({
             <div className="fixed inset-0 bg-black bg-opacity-25" />
           </Transition.Child>
 
-          <div className="fixed inset-0 overflow-hidden border-black border-2">
-            <div className="flex min-h-full items-center justify-center px-6 border-black border-2">
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="flex min-h-full items-center justify-center px-6">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -121,24 +121,24 @@ const SingleTicket = ({
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Dialog.Panel className="relative w-full max-w-full h-[70vh] min-h-full max-h-[80vh] transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex-col border-black border-2">
-                  <button
-                    className="absolute top-2 right-2 z-10 w-fit pt-2 bg-primary-blue-100 rounded-full"
-                    onClick={closeModal}
-                    type="button"
-                  >
-                    <Image
-                      alt="close"
-                      className="object-contain"
-                      height={20}
-                      src="/close.svg"
-                      width={20}
-                    />
-                  </button>
-
-                  <div className="flex justify-center bg-gray-200">
+                <Dialog.Panel className="relative w-full max-w-full h-[70vh] min-h-full max-h-[80vh] transform rounded-2xl bg-white text-left shadow-xl transition-all flex-col">
+                  <div className="grid grid-cols-3 pt-2 px-2 border-black border-b-2 min-h-[10%]">
+                    <button
+                      className="absolute top-2 right-2 pt-2 z-10 bg-primary-blue-100 rounded-full"
+                      onClick={closeModal}
+                      type="button"
+                    >
+                      <Image
+                        alt="close"
+                        className="object-contain"
+                        height={20}
+                        src="/close.svg"
+                        width={20}
+                      />
+                    </button>
+                    <div className="self-center">Created by </div>
                     <input
-                      className="text-2xl text-center font-semibold bg-gray-200"
+                      className="text-4xl text-center font-bold overflow-auto"
                       onChange={e => handleChangeTitle(e.target.value)}
                       onBlur={e => {
                         setFlag();
@@ -148,14 +148,45 @@ const SingleTicket = ({
                     />
                   </div>
 
-                  <div>
-                    <p>{ticket.description}</p>
+                  <div className="border-black border-t-2 grid grid-cols-3 px-2 min-h-[10%]">
+                    <div className="self-center">Assignees: </div>
+                    <div className="place-self-center">
+                      Deadline:
+                      {updatedTicket.deadline ? (
+                        <span> 20-12-2023</span>
+                      ) : (
+                        <span> none</span>
+                      )}
+                    </div>
+                    <div className="place-self-center">GAM</div>
                   </div>
 
-                  <div>
-                    {ticket.comments?.map((comment, index) => (
-                      <SingleComment comment={comment} key={index} />
-                    ))}
+                  <div className="grid grid-cols-3 w-full min-h-[80%]">
+                    <div className="col-span-2">
+                      <div className="grid grid-rows-3 min-h-full">
+                        <div className="row-span-2 mr-1 ml-4 my-1 bg-gray-200 border-black border-2 rounded-2xl">
+                          <h1 className="text-3xl font-semibold text-center">
+                            Description
+                          </h1>
+                          <p>{updatedTicket.description}</p>
+                        </div>
+
+                        <div className="mr-1 ml-4 mt-1 mb-2 bg-gray-200 border-black border-2 rounded-2xl">
+                          <h1 className="text-3xl font-semibold text-center">
+                            Comments
+                          </h1>
+                          {updatedTicket.comments?.map((comment, index) => (
+                            <SingleComment comment={comment} key={index} />
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="ml-1 mr-4 mt-1 mb-2 bg-gray-200 border-black border-2 rounded-2xl">
+                      <h1 className="text-3xl font-semibold text-center">
+                        Relationship tree
+                      </h1>
+                    </div>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
