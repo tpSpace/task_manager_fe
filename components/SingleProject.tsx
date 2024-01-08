cb41c3eb74643e6d3dcf5d0231c92e35a469a933
Nguyễn Mạch Khang Huy <itcsiu21072@student.hcmiu.edu.vn>
2024-01-08 13:19:19 +0700

    Huy|Edit|PJ UI

--- components/SingleProject.tsx
+++ components/SingleProject.tsx
@@ -18,44 +18,48 @@ const SingleProject = ({
   setStageChangingFlag,
 }: SingleProjectProps) => {
   return (
-    <div className="w-full h-full flex flex-row border-2">
-      <div className="w-[15%] min-w-[150px] h-full flex flex-col border border-black-100">
-        <div className="flex flex-col space-y-3 justify-center items-center h-30 mt-20">
-          <div className="border-2 border-black rounded-full h-10 w-full bg-black flex items-center justify-center">
-            <p className="text-white text-xl">Task</p>
+    <div className="w-full h-full flex flex-row justify-center items-center border-r-2">
+      <div className="w-[15%] min-w-[150px] h-full flex-col items-center flex bg-neutral-300 relative">
+        <div className=" space-y-3 h-30 w-1/2 mt-12 ">
+          <div className="border-2 rounded-xl h-10 w-full bg-neutral-400 border-neutral-500 flex items-center justify-center">
+            <p className="text-white text-xl font-Roboto font-medium">Task</p>
           </div>
-          <div className="border-2 border-black rounded-full h-10 w-full bg-black flex items-center justify-center">
-            <p className="text-white text-xl">Timeline</p>
+          <div className="border-2 rounded-xl h-10 w-full bg-neutral-400 border-neutral-500 flex items-center justify-center">
+            <p className="text-white text-xl font-Roboto font-medium">Timeline</p>
           </div>
         </div>
 
         <div className="flex flex-col justify-center items-center py-2 space-y-4 mt-20">
-          <div className="text-lg">Members ({project.members.length})</div>
-          <div className="flex flex-col space-y-4  ">
+          <div className="text-2xl font-Montserrat font-semibold">Members ({project.members.length})</div>
+          <div className="flex flex-col space-y-4 ">
             {project.members.map((member, index) => (
               <UserCard key={index} user={member} />
             ))}
           </div>
         </div>
-        <div className="text-lg text-center flex-wrap flex flex-end mt-10">
-          {' '}
-          Copy this code <br /> to add member
-          <br /># 1234
+        <div className="mt-4 w-full text-center">
+          <span className={"text-lg font-Roboto"}>
+            Copy this code to invite
+          </span>
+          <p className={"text-sm font-Roboto font-bold truncate select-all text-cyan-400 hover:text-cyan-500"}>
+            {project.id}
+          </p>
         </div>
+
       </div>
       <div className="flex flex-row w-full h-full overflow-x-auto overflow-y-hidden">
         <div className="w-full h-full">
-          <div className="bg-gray-100 h-[10%] flex items-center px-6 justify-between">
+          <div className="bg-neutral-100 h-[10%] flex items-center px-6 justify-between">
             <div className="flex">
               <Link
-                className="bg-slate-800 text-white px-4 py-1 rounded-lg w-fit hover:bg-slate-200 hover:text-black"
+                className="bg-neutral-300 border-2 border-neutral-400 text-neutral-600 px-4 py-1 rounded-xl w-fit hover:bg-neutral-400"
                 href={'/projects'}
               >
                 <BsArrowLeft className="text-4xl" />
               </Link>
             </div>
-            <div className="font-bold text-4xl">{project.title}</div>
-            <div>Setting</div>
+            <div className="font-bold font-Montserrat text-4xl">{project.title}</div>
+            <div className={"font-Roboto font-medium"}>Setting</div>
           </div>
           <ListStages
             project={project}
